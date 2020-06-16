983b11320f2bdf60413c27007b69f6d8ad1a410c
Joshua Ciffer <Josh@Cyph-Desktop>
2020-06-16 03:40:31 -0400

    Initial game setup completed

--- src/undoRedoButtonUsingAStack/BoardGame_3.java
+++ src/undoRedoButtonUsingAStack/BoardGame_3.java
@@ -19,30 +19,34 @@ public class BoardGame_3 extends Application {
 	private static final int TURN_WHITE = 1;
 
 	// Game image objects
-	Image emptyImage = new Image("file:emptySquare80.gif");
-	Image blackImage = new Image("file:blackPiece80.gif");
-	Image whiteImage = new Image("file:whitePiece80.gif");
-	Image blackMoveImage = new Image("file:blackMoveSquare80.gif");
-	Image whiteMoveImage = new Image("file:whiteMoveSquare80.gif");
+	Image emptyImage = new Image("file:D:\\Programming\\Projects\\CS114 - Intro to CS II\\src\\undoRedoButtonUsingAStack\\emptySquare80.gif");
+	Image blackImage = new Image("file:D:\\Programming\\Projects\\CS114 - Intro to CS II\\src\\undoRedoButtonUsingAStack\\blackPiece80.gif");
+	Image whiteImage = new Image("file:D:\\Programming\\Projects\\CS114 - Intro to CS II\\src\\undoRedoButtonUsingAStack\\whitePiece80.gif");
+	Image blackMoveImage = new Image("file:D:\\Programming\\Projects\\CS114 - Intro to CS II\\src\\undoRedoButtonUsingAStack\\blackMoveSquare80.gif");
+	Image whiteMoveImage = new Image("file:D:\\Programming\\Projects\\CS114 - Intro to CS II\\src\\undoRedoButtonUsingAStack\\whiteMoveSquare80.gif");
 
 	// Game image objects
 	double imageWidth = emptyImage.getWidth();
 	double imageHeight = emptyImage.getHeight();
 
+	// Current player turn
 	private int turn = TURN_BLACK;
 
-	private Image im_move_vec[] = {blackImage, whiteImage};
-	private Image im_mark_vec[] = {blackMoveImage, whiteMoveImage};
+	// Categorized images
+	private Image moveImages[] = {blackImage, whiteImage};
+	private Image selectImages[] = {blackMoveImage, whiteMoveImage};
 
-	Image im_on_move = im_move_vec[turn];
-	Image im_off_move = im_move_vec[1 - turn];
+	// Specified image getters
+	Image currentTurnImage = moveImages[turn];
+	Image previousTurnImage = moveImages[1 - turn];
 
 	// Game canvas
-	Canvas canvas = new Canvas(imageWidth * 8, imageHeight * 8);
+	Canvas canvas = new Canvas(imageWidth * NUM_ROWS, imageHeight * NUM_COLS);
 	GraphicsContext graphicsContext = canvas.getGraphicsContext2D();
 
+	// Entry point
 	public static void main(String[] args) {
-		launch(BoardGame_3.class, args);
+		launch(args);
 	}
 
 	// Game screen size
@@ -52,9 +56,10 @@ public class BoardGame_3 extends Application {
 	// Game board
 	private Image board[][] = new Image[NUM_ROWS][NUM_COLS];
 	private final Group group = new Group(canvas);
-	private final Scene scene = new Scene(group, imageWidth * NUM_COLS, imageHeight * NUM_ROWS, Color.BLACK);
+	private final Scene scene = new Scene(group, imageWidth * NUM_COLS, imageHeight * NUM_ROWS);
 
-	private Image boardContents(int row, int col) {
+	// Returns image at specified board location
+	private Image getBoardContent(int row, int col) {
 		if (row < 0 || row > 7)
 			return null;
 		if (col < 0 || col > 7)
@@ -66,20 +71,20 @@ public class BoardGame_3 extends Application {
 	@Override
 	public void start(Stage stage) throws Exception {
 
-		final Group group = new Group(canvas);
-		final Scene scene = new Scene(group, imageWidth * NUM_COLS, imageHeight * NUM_ROWS, Color.BLACK);
-
-		for (int row = 0; row < NUM_ROWS; row++)
+		for (int row = 0; row < NUM_ROWS; row++) {
 			for (int col = 0; col < NUM_COLS; col++) {
 				graphicsContext.drawImage(emptyImage, col * imageWidth, row * imageHeight);
 				board[row][col] = emptyImage;
 			}
-		// --------------------------------------------------------------------
+		}
 		// Put code here to setup initial position
-		//
-
+		graphicsContext.drawImage(whiteImage, 3 * imageWidth, 4 * imageHeight);
+		graphicsContext.drawImage(blackImage, 4 * imageWidth, 4 * imageHeight);
+		graphicsContext.drawImage(blackImage, 3 * imageWidth, 3 * imageHeight);
+		graphicsContext.drawImage(whiteImage, 4 * imageWidth, 3 * imageHeight);
+		
 		stage.setScene(scene);
-		stage.setTitle("Othello");
+		stage.setTitle("Board Game 3");
 		stage.show();
 
 		// Handle Mouse Events
@@ -93,11 +98,11 @@ public class BoardGame_3 extends Application {
 				// Check clicked square is empty.
 				// Compute flips. Not a valid move if no flips.
 				//
-				graphicsContext.drawImage(im_on_move, col * imageWidth, row * imageHeight);
-				board[row][col] = im_on_move;
+				graphicsContext.drawImage(currentTurnImage, col * imageWidth, row * imageHeight);
+				board[row][col] = currentTurnImage;
 				turn = 1 - turn;
-				im_on_move = im_move_vec[turn];
-				im_off_move = im_move_vec[1 - turn];
+				currentTurnImage = moveImages[turn];
+				previousTurnImage = moveImages[1 - turn];
 				System.out.println((int)(e.getY() / imageHeight) + ", " + (int)(e.getX() / imageWidth));
 			}
 		};
@@ -106,7 +111,6 @@ public class BoardGame_3 extends Application {
 	}
 
 	private class Move {
-
 		int turn;
 		int row_move;
 		int col_move;
